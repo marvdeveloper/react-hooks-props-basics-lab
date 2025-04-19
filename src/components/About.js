4e67bfe0d10c7740a5a76548a58a6efb2599a34f
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-04-19 17:48:48 +0300

    Update About.js to conditionally render bio and pass props to Links component

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Links from "./Links"; // Import the Links component
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {props.bio && <p>{props.bio}</p>} {/* Conditionally render bio */}
+      <img src={props.image} alt="I made this" />
+      <Links github={props.github} linkedin={props.linkedin} /> {/* Pass props */}
     </div>
   );
 }
