cf4e7ff1fb0f2a46be6410b04c00539d2fcaffb6
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-04-19 17:50:09 +0300

    Pass user data as props to Home, About, and Links components in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,19 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color={user.color} />
+      <About
+        bio={user.bio}
+        image={user.image} // if you add image in your user object
+        github={user.links.github} // Accessing github link
+        linkedin={user.links.linkedin} // Accessing linkedin link
+      />
     </div>
   );
 }
